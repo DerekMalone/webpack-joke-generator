40293d32fa728fbd0e6832a2c915b3d1ff52db49
DerekMalone <laxmalone@gmail.com>
2021-10-04 21:41:04 -0500

    styling for app

--- src/scripts/views/landingPage.js
+++ src/scripts/views/landingPage.js
@@ -1,6 +1,6 @@
 const landingPage = () => {
   document.querySelector('#button-container').innerHTML = `
-    <div><button id="get-joke-btn" class="btn btn-get-joke-btn btn-info">Get Joke!</button></div>;
+    <div><button id="get-joke-btn" class="btn btn-get-joke-btn btn-info">Get Joke!</button></div>
   `;
 };
 
--- src/scripts/views/punchline.js
+++ src/scripts/views/punchline.js
@@ -1,10 +1,11 @@
-const punchline = (obj) => {
+const punchline = (obj = {}) => {
   document.querySelector('#joke-container').innerHTML = `
     <p>${obj.setup}</p>
+    <hr>
     <p>${obj.punchline}</p>
     `;
   document.querySelector('#button-container').innerHTML = `
-      <div><button id="get-new-joke-btn" class="btn btn-get-punchline-btn btn-info">Get New Joke!</button></div>;
+      <div><button id="get-new-joke-btn" class="btn btn-get-punchline-btn btn-info">Get New Joke!</button></div>
       `;
 };
 
